47bbb90c059b434271b3b1000f9a5ed4251ed45d
Lee <39526340+candidlyfrank@users.noreply.github.com>
2026-07-07 20:26:55 +0100

    Add variable to rendered template

--- src/django/app/views.py
+++ src/django/app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+_globalVars = {
+    'name': "John Smith"
+}
+
 def say_hello(request: HttpResponse) -> HttpResponse:
-    return render(request, "hello.html")
+    return render(request, "hello.html", _globalVars)
