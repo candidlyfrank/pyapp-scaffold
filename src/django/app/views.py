e3cd7cba46c74a88aae2d7f76315b0faaa09bb13
Lee <39526340+candidlyfrank@users.noreply.github.com>
2026-07-07 20:22:05 +0100

    Render simple template from route

--- src/django/app/views.py
+++ src/django/app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request: HttpResponse) -> HttpResponse:
-    return HttpResponse("Hello, world!")
+    return render(request, "hello.html")
