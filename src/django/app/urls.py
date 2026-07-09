eac943df9ae36277f68bc2979ebcde9c277913ef
Lee <39526340+candidlyfrank@users.noreply.github.com>
2026-07-09 22:30:05 +0100

    Add "playground" view

--- src/django/app/urls.py
+++ src/django/app/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("health/", health, name="health"),
+    path("playground/", home, name="home"),
     path("playground/hello/", views.say_hello, name="hello-world"),
 ]
 
